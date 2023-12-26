5c3ac8eba559c700e4f5f74b5faaceab77ef73c6
transfer_kk <kseysh123@naver.com>
2023-12-26 17:25:34 +0900

    feat - 프로젝트 초기 세팅

---
+++ HMH/src/main/java/sopt/org/HMH/HmhApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.HMH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmhApplication.class, args);
+	}
+
+}
