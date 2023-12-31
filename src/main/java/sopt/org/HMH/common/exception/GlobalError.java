6370754133ee3e067e1cd772d0ec2c918d7c1b49
transfer_kk <kseysh123@naver.com>
2023-12-31 15:34:18 +0900

    feat - #3 예외 핸들링 클래스 구현 및 공통 응답 클래스 구현

---
+++ src/main/java/sopt/org/HMH/common/exception/GlobalError.java
@@ -0,0 +1,33 @@
+package sopt.org.HMH.common.exception;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import sopt.org.HMH.common.exception.base.ErrorBase;
+
+@AllArgsConstructor
+public enum GlobalError implements ErrorBase {
+
+    ;
+
+    private final HttpStatus status;
+    private final String errorMessage;
+
+
+    @Override
+    public int getHttpStatusCode() {
+        return status.value();
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return this.status;
+    }
+
+
+    @Override
+    public String getErrorMessage() {
+        return this.errorMessage;
+    }
+
+}
---
+++ src/main/java/sopt/org/HMH/common/exception/GlobalException.java
@@ -0,0 +1,13 @@
+package sopt.org.HMH.common.exception;
+
+import sopt.org.HMH.common.exception.base.ExceptionBase;
+
+/**
+ * 전역적 혹은 시스템 전체 범위에서 발생하는 예외
+ */
+public class GlobalException extends ExceptionBase {
+
+    public GlobalException(GlobalError error) {
+        super(error);
+    }
+}
