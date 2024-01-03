45334d94d4f5bd1096dfc471d3a00116c95de6b0
임주민 <limj1213@gmail.com>
2024-01-04 03:32:08 +0900

    feat - #9 챌린지 도메인 구현

---
+++ src/main/java/sopt/org/HMH/common/domain/Challenge.java
@@ -0,0 +1,42 @@
+package sopt.org.HMH.common.domain;
+
+import static jakarta.persistence.FetchType.*;
+import static jakarta.persistence.GenerationType.*;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Challenge extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "challenge_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Integer period;
+    private Long goalTime;
+
+    @OneToMany
+    private List<DayChallenge> dayChallenges;
+
+    @Builder
+    public Challenge(User user, Integer period, Long goalTime, DayChallenge dayChallenges) {
+        this.period = period;
+        this.goalTime = goalTime;
+        this.user = user;
+        this.dayChallenges.add(dayChallenges);
+        this.createdAt = LocalDateTime.now();
+    }
+}
