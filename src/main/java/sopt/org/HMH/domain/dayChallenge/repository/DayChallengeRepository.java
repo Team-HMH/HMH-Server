a24a6a142f4921283767cfb7ede3e586ea9bd00f
임주민 <limj1213@gmail.com>
2024-01-09 14:59:05 +0900

    feat - #9 exception 유형 변경

--- src/main/java/sopt/org/HMH/domain/dayChallenge/repository/DayChallengeRepository.java
+++ src/main/java/sopt/org/HMH/domain/dayChallenge/repository/DayChallengeRepository.java
@@ -3,11 +3,14 @@ package sopt.org.HMH.domain.dayChallenge.repository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sopt.org.HMH.domain.dayChallenge.domain.DayChallenge;
-import sopt.org.HMH.domain.user.exception.UserError;
+import sopt.org.HMH.domain.dayChallenge.domain.exception.DayChallengeError;
 
 public interface DayChallengeRepository extends JpaRepository<DayChallenge, Long> {
 
     default DayChallenge findByIdOrThrowException(Long dayChallengeId) {
-        return findById(dayChallengeId).orElseThrow(() -> new EntityNotFoundException(UserError.USER_NOT_FOUND.getErrorMessage()));
+        return findById(dayChallengeId).orElseThrow(() -> new EntityNotFoundException(DayChallengeError.CHALLENGE_NOT_FOUND.getErrorMessage()));
     }
 }
+
+
+
