165e861c78216b65d09def1452d75a49be9020b4
transfer_kk <kseysh123@naver.com>
2024-01-09 13:18:05 +0900

    del - #18 사용하지 않는 파일 삭제

--- src/main/java/sopt/org/HMH/domain/user/dto/request/SocialLoginRequest.java
+++
@@ -1,8 +0,0 @@
-package sopt.org.HMH.domain.user.dto.request;
-
-import sopt.org.HMH.global.auth.social.SocialPlatform;
-
-public record SocialLoginRequest(
-        SocialPlatform socialPlatform
-) {
-}
