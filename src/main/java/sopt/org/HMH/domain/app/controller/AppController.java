437ea9bb714d4a830dbbdff3c98c5d031dd7e522
임주민 <limj1213@gmail.com>
2024-01-10 19:39:39 +0900

    feat - #15 appController 구현

--- src/main/java/sopt/org/HMH/domain/app/controller/AppController.java
+++ src/main/java/sopt/org/HMH/domain/app/controller/AppController.java
@@ -1,11 +1,41 @@
 package sopt.org.HMH.domain.app.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sopt.org.HMH.domain.app.domain.exception.AppSuccess;
+import sopt.org.HMH.domain.app.dto.request.AppArrayGoalTimeRequest;
+import sopt.org.HMH.domain.app.dto.request.AppDeleteRequest;
+import sopt.org.HMH.domain.app.service.AppService;
+import sopt.org.HMH.global.common.response.ApiResponse;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/app")
 public class AppController {
+
+    private final AppService appService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse> orderAdd(
+            @RequestHeader("os") String os,
+            @RequestBody AppArrayGoalTimeRequest request
+    ) {
+        // TODO : - 1L -> principal로 변경
+        appService.addAppsByUserId(1L, request.apps());
+        return ResponseEntity
+                .status(AppSuccess.SUCCESS_ADD_APP.getHttpStatus())
+                .body(ApiResponse.success(AppSuccess.SUCCESS_ADD_APP));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<ApiResponse> orderRemove(
+            @RequestHeader("os") String os,
+            @RequestBody AppDeleteRequest request
+    ) {
+        appService.removeApp(1L, request);
+        return ResponseEntity
+                .status(AppSuccess.SUCCESS_DELETE_APP.getHttpStatus())
+                .body(ApiResponse.success(AppSuccess.SUCCESS_DELETE_APP));
+    }
 }
