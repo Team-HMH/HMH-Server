1f0fdd53da6403552dcf14a7f2abecef5da940df
임주민 <limj1213@gmail.com>
2024-01-11 14:17:25 +0900

    feat - #16 os정보도 추가로 조회해서 앱 삭제

--- src/main/java/sopt/org/HMH/domain/app/repository/AppRepository.java
+++ src/main/java/sopt/org/HMH/domain/app/repository/AppRepository.java
@@ -5,5 +5,5 @@ import sopt.org.HMH.domain.app.domain.App;
 
 public interface AppRepository extends JpaRepository<App, Long> {
 
-    App findByDailyChallengeIdAndAppCode(Long dayChallengeId, String appCode);
+    App findByDailyChallengeIdAndAppCodeAndOs(Long dayChallengeId, String appCode, String os);
 }
