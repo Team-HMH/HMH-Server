91bdbed544edcb27ee863685d3cdf12dfc192065
임주민 <limj1213@gmail.com>
2024-01-11 09:33:02 +0900

    refactor - #15 와일드카드 삭제

--- src/main/java/sopt/org/HMH/domain/app/domain/App.java
+++ src/main/java/sopt/org/HMH/domain/app/domain/App.java
@@ -1,7 +1,15 @@
 package sopt.org.HMH.domain.app.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sopt.org.HMH.domain.dailychallenge.domain.DailyChallenge;
