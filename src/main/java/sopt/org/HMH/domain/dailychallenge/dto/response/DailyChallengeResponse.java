e0e5e26cc8d4a6a975de33f12cf6d1cca54ac0b4
임주민 <limj1213@gmail.com>
2024-01-11 20:54:49 +0900

    feat - #19 response 파일 생성

--- src/main/java/sopt/org/HMH/domain/dailychallenge/dto/response/DailyChallengeResponse.java
+++ src/main/java/sopt/org/HMH/domain/dailychallenge/dto/response/DailyChallengeResponse.java
@@ -1,10 +1,33 @@
 package sopt.org.HMH.domain.dailychallenge.dto.response;
 
-import sopt.org.HMH.domain.app.dto.request.AppArrayGoalTimeRequest;
+import lombok.AccessLevel;
+import lombok.Builder;
+import sopt.org.HMH.domain.app.domain.App;
+import sopt.org.HMH.domain.dailychallenge.domain.DailyChallenge;
 
+import java.util.List;
+
+@Builder(access = AccessLevel.PRIVATE)
 public record DailyChallengeResponse(
         String status,
         Long goalTime,
-        AppArrayGoalTimeRequest apps
+        List<AppGoalTimeResponse> apps
 ) {
+    public static DailyChallengeResponse of(DailyChallenge dailyChallenge) {
+        return DailyChallengeResponse.builder()
+                .status(dailyChallenge.getStatus().toString())
+                .status(dailyChallenge.getStatus().toString())
+                .goalTime(dailyChallenge.getGoalTime())
+                .apps(dailyChallenge.getApps().stream().map(AppGoalTimeResponse::of).toList())
+                .build();
+    }
+
+    public record AppGoalTimeResponse(
+            String appCode,
+            Long goalTime
+    ) {
+        static AppGoalTimeResponse of(App app) {
+            return new AppGoalTimeResponse(app.getAppCode(), app.getGoalTime());
+        }
+    }
 }
