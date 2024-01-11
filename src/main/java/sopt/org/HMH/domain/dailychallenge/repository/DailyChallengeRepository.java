a78d4ef30a94ad2b41d3a2422d8e917c21ec48da
임주민 <limj1213@gmail.com>
2024-01-11 14:18:11 +0900

    chore - #16 필요없는 import문 삭제

--- src/main/java/sopt/org/HMH/domain/dailychallenge/repository/DailyChallengeRepository.java
+++ src/main/java/sopt/org/HMH/domain/dailychallenge/repository/DailyChallengeRepository.java
@@ -1,12 +1,9 @@
 package sopt.org.HMH.domain.dailychallenge.repository;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
-import sopt.org.HMH.domain.challenge.domain.exception.ChallengeException;
 import sopt.org.HMH.domain.dailychallenge.domain.DailyChallenge;
 import sopt.org.HMH.domain.dailychallenge.domain.exception.DailyChallengeError;
 import sopt.org.HMH.domain.dailychallenge.domain.exception.DailyChallengeException;
-import sopt.org.HMH.domain.user.domain.exception.UserError;
 
 public interface DailyChallengeRepository extends JpaRepository<DailyChallenge, Long> {
 
