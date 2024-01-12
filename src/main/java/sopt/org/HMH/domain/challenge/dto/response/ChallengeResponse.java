237f683dee7feb63b606ba4b7aaeeb037559edc2
임주민 <limj1213@gmail.com>
2024-01-12 17:53:50 +0900

    feat - #20 ChallengeResponse 정의

--- src/main/java/sopt/org/HMH/domain/challenge/dto/response/ChallengeResponse.java
+++ src/main/java/sopt/org/HMH/domain/challenge/dto/response/ChallengeResponse.java
@@ -1,2 +1,29 @@
-package sopt.org.HMH.domain.challenge.dto.response;public record ChallengeResponse() {
+package sopt.org.HMH.domain.challenge.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import sopt.org.HMH.domain.app.dto.response.AppGoalTimeResponse;
+import sopt.org.HMH.domain.challenge.domain.Challenge;
+import sopt.org.HMH.domain.dailychallenge.domain.DailyChallenge;
+import sopt.org.HMH.domain.dailychallenge.domain.Status;
+
+import java.util.List;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record ChallengeResponse(
+        Integer period,
+        List<Status> statuses,
+        Integer todayIndex,
+        Long goalTime,
+        List<AppGoalTimeResponse> apps
+) {
+    public static ChallengeResponse of(Challenge challenge, DailyChallenge dailyChallenge, List<Status> statuses, Integer todayIndex) {
+        return ChallengeResponse.builder()
+                .period(challenge.getPeriod())
+                .statuses(statuses)
+                .todayIndex(todayIndex)
+                .goalTime(dailyChallenge.getGoalTime())
+                .apps(dailyChallenge.getApps().stream().map(AppGoalTimeResponse::of).toList())
+                .build();
+    }
 }
