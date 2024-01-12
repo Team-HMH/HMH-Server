abeb0268b4952ad7ee1d040937f9a07175a35fe6
transfer_kk <kseysh123@naver.com>
2024-01-12 17:47:34 +0900

    del - #38 병합 때 사용되지 않게 된 필드 삭제

--- src/main/java/sopt/org/HMH/domain/challenge/service/ChallengeService.java
+++ src/main/java/sopt/org/HMH/domain/challenge/service/ChallengeService.java
@@ -3,26 +3,19 @@ package sopt.org.HMH.domain.challenge.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sopt.org.HMH.domain.app.service.AppService;
-import org.springframework.transaction.annotation.Transactional;
 import sopt.org.HMH.domain.challenge.domain.Challenge;
 import sopt.org.HMH.domain.challenge.dto.request.ChallengeRequest;
 import sopt.org.HMH.domain.challenge.dto.response.AddChallengeResponse;
 import sopt.org.HMH.domain.challenge.repository.ChallengeRepository;
 import sopt.org.HMH.domain.dailychallenge.service.DailyChallengeService;
-import sopt.org.HMH.domain.user.domain.User;
-import sopt.org.HMH.domain.user.repository.UserRepository;
-import sopt.org.HMH.domain.user.service.UserService;
 
 @Service
 @RequiredArgsConstructor
 public class ChallengeService {
 
     private final ChallengeRepository challengeRepository;
-    private final UserRepository userRepository;
     private final DailyChallengeService dailyChallengeService;
-    private final AppService appService;
-    private final UserService userService;
+
 
     @Transactional
     public AddChallengeResponse addChallenge(Long userId,
