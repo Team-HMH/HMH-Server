19dad82737bd4406ff25a3fe8767c6989b6bc0b8
transfer_kk <kseysh123@naver.com>
2024-01-13 23:51:37 +0900

    chore - #47 공통 Constants로 사용

---
+++ src/main/java/sopt/org/HMH/global/common/constant/Constants.java
@@ -0,0 +1,7 @@
+package sopt.org.HMH.global.common.constant;
+
+public abstract class Constants {
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+    public static final Integer INITIAL_POINT = 0;
+}
