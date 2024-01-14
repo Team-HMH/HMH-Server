699e03ba5999a7afb9b820b420802e174f1bbc7d
임주민 <limj1213@gmail.com>
2024-01-14 15:08:58 +0900

    refactor - #20 비즈니스 로직만 남아있도록 코드 삭제

--- src/main/java/sopt/org/HMH/domain/challenge/service/ChallengeService.java
+++ src/main/java/sopt/org/HMH/domain/challenge/service/ChallengeService.java
@@ -25,34 +25,19 @@ public class ChallengeService {
 
     private final ChallengeRepository challengeRepository;
     private final DailyChallengeService dailyChallengeService;
-    private final DailyChallengeRepository dailyChallengeRepository;
 
     @Transactional
     public AddChallengeResponse addChallenge(Long userId, ChallengeRequest request, String os) {
         Challenge challenge = challengeRepository.save(Challenge.builder()
                         .period(request.period())
                         .userId(userId).build());
-        for (int count = 0; count <= request.period(); count++)
-            dailyChallengeService.addDailyChallenge(challenge, request.goalTime(), request.apps(), os);
-
+        dailyChallengeService.addDailyChallengesForPeriod(challenge, request, os);
         return AddChallengeResponse.of(challenge.getId());
     }
 
     public ChallengeResponse getChallenge(Long userId, String os) {
         val challenge = challengeRepository.findFirstByUserIdOrderByCreatedAtDesc(userId);
-        val dailyChallenges = challenge.getDailyChallenges();
 
-        val statuses = new ArrayList<Status>();
-        for (val dailyChallenge : dailyChallenges) {
-            statuses.add(dailyChallenge.getStatus());
-        }
-
-        val startDayOfChallenge = challenge.getDailyChallenges().get(0);
-        val todayIndex = calculateTodayDailyChallengeIndex(startDayOfChallenge.getCreatedAt());
-        return ChallengeResponse.of(challenge, dailyChallenges.get(todayIndex), statuses, todayIndex);
-    }
-
-    private Integer calculateTodayDailyChallengeIndex(LocalDateTime startDateOfChallenge) {
-        return (int) ChronoUnit.DAYS.between(LocalDateTime.now().toLocalDate(), startDateOfChallenge.toLocalDate());
+        return ChallengeResponse.of(challenge);
     }
 }
