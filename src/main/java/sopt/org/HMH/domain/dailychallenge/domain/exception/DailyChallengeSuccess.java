5ed12735fa0e344cff706bf41a4d141886e2fd4a
임주민 <limj1213@gmail.com>
2024-01-15 22:07:22 +0900

    feat - #42 성공 메시지 상수 추가

--- src/main/java/sopt/org/HMH/domain/dailychallenge/domain/exception/DailyChallengeSuccess.java
+++ src/main/java/sopt/org/HMH/domain/dailychallenge/domain/exception/DailyChallengeSuccess.java
@@ -8,7 +8,8 @@ import sopt.org.HMH.global.common.exception.base.SuccessBase;
 public enum DailyChallengeSuccess implements SuccessBase {
 
     GET_DAILY_CHALLENGE_SUCCESS(HttpStatus.OK, "오늘의 일별 챌린지 이용 통계 조회를 성공하였습니다."),
-    MODIFY_DAILY_CHALLENGE_STATUS_SUCCESS(HttpStatus.OK, "오늘의 일별 챌린지 STATUS 실패 처리를 성공하였습니다"),
+    MODIFY_DAILY_CHALLENGE_STATUS_SUCCESS(HttpStatus.OK, "오늘의 일별 챌린지 STATUS 변경 업데이트를 성공하였습니다"),
+    MODIFY_DAILY_CHALLENGE_STATUS_FAILURE_SUCCESS(HttpStatus.OK, "오늘의 일별 챌린지 STATUS 실패 처리를 성공하였습니다"),
     ;
 
     private final HttpStatus status;
