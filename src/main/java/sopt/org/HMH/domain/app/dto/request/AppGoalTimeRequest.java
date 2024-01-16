c7d3f284d7092e1ebe8aa9fb9ecf0f6a907c08a6
임주민 <limj1213@gmail.com>
2024-01-16 19:41:19 +0900

    feat - #51 App에서 AppGoalTimeRequest로 형식 변환하는 메소드 구현

--- src/main/java/sopt/org/HMH/domain/app/dto/request/AppGoalTimeRequest.java
+++ src/main/java/sopt/org/HMH/domain/app/dto/request/AppGoalTimeRequest.java
@@ -1,7 +1,13 @@
 package sopt.org.HMH.domain.app.dto.request;
 
+import sopt.org.HMH.domain.app.domain.App;
+import sopt.org.HMH.domain.app.dto.response.AppGoalTimeResponse;
+
 public record AppGoalTimeRequest(
         String appCode,
         Long goalTime
 ) {
+    public static AppGoalTimeRequest of(App app) {
+        return new AppGoalTimeRequest(app.getAppCode(), app.getGoalTime());
+    }
 }
