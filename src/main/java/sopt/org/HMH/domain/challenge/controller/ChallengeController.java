0535fe17b7277a85f4dcd8632dec5454e1085733
임주민 <limj1213@gmail.com>
2024-01-16 19:43:03 +0900

    feat - #51 챌린지 생성 시 과거 유저가 추가했던 앱 정보 불러와주는 기능 추가 (회원가입 시 챌린지 생성과 구분)

--- src/main/java/sopt/org/HMH/domain/challenge/controller/ChallengeController.java
+++ src/main/java/sopt/org/HMH/domain/challenge/controller/ChallengeController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sopt.org.HMH.domain.challenge.domain.exception.ChallengeSuccess;
 import sopt.org.HMH.domain.challenge.dto.request.ChallengeRequest;
-import sopt.org.HMH.domain.challenge.dto.response.AddChallengeResponse;
 import sopt.org.HMH.domain.challenge.dto.response.ChallengeResponse;
 import sopt.org.HMH.domain.challenge.service.ChallengeService;
 import sopt.org.HMH.global.common.response.ApiResponse;
@@ -28,8 +27,9 @@ public class ChallengeController {
 
     @PostMapping
     public ResponseEntity<ApiResponse<?>> orderAddChallenge(Principal principal,
+                                                            @RequestHeader("OS") final String os,
                                                             @RequestBody final ChallengeRequest request) {
-        challengeService.addChallenge(IdConverter.getUserId(principal), request.period(), request.goalTime());
+        challengeService.addChallengeWithPastInfo(IdConverter.getUserId(principal), request.period(), request.goalTime(), os);
         return ResponseEntity
                 .status(ChallengeSuccess.ADD_CHALLENGE_SUCCESS.getHttpStatus())
                 .body(ApiResponse.success(ChallengeSuccess.ADD_CHALLENGE_SUCCESS, new EmptyJsonResponse()));
--- src/main/java/sopt/org/HMH/domain/challenge/service/ChallengeService.java
+++ src/main/java/sopt/org/HMH/domain/challenge/service/ChallengeService.java
@@ -4,11 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sopt.org.HMH.domain.app.dto.request.AppGoalTimeRequest;
+import sopt.org.HMH.domain.app.service.AppService;
 import sopt.org.HMH.domain.challenge.domain.Challenge;
-import sopt.org.HMH.domain.challenge.dto.response.AddChallengeResponse;
 import sopt.org.HMH.domain.challenge.dto.response.ChallengeResponse;
 import sopt.org.HMH.domain.challenge.repository.ChallengeRepository;
+import sopt.org.HMH.domain.dailychallenge.domain.DailyChallenge;
 import sopt.org.HMH.domain.dailychallenge.service.DailyChallengeService;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +20,7 @@ public class ChallengeService {
 
     private final ChallengeRepository challengeRepository;
     private final DailyChallengeService dailyChallengeService;
+    private final AppService appService;
 
     @Transactional
     public Challenge addChallenge(Long userId, Integer period, Long goalTime) {
@@ -23,12 +28,43 @@ public class ChallengeService {
                         .period(period)
                         .goalTime(goalTime)
                         .userId(userId).build());
-        dailyChallengeService.addDailyChallengesForPeriod(challenge, period, goalTime);
+
         return challenge;
+    }
+
+    @Transactional
+    public Challenge addChallengeWithInfo(Long userId, Integer period, Long goalTime, List<AppGoalTimeRequest> apps, String os) {
+        Challenge challenge = addChallenge(userId, period, goalTime);
+        for (int count = 0; count < challenge.getPeriod(); count++) {
+            DailyChallenge dailyChallenge = dailyChallengeService.addDailyChallenge(challenge);
+            appService.addApps(dailyChallenge, apps, os);
+        }
+
+        return challenge;
+    }
+
+    @Transactional
+    public Challenge addChallengeWithPastInfo(Long userId, Integer period, Long goalTime, String os) {
+        List<AppGoalTimeRequest> lastApps = getLastApps(userId);
+
+        return addChallengeWithInfo(userId, period, goalTime, lastApps, os);
+    }
+
+    private List<AppGoalTimeRequest> getLastApps(Long userId) {
+        Challenge lastChallenge = challengeRepository.findFirstByUserIdOrderByCreatedAtDesc(userId);
+        int lastIndexOfDailyChallenge = lastChallenge.getDailyChallenges().size()-1;
+        List<AppGoalTimeRequest> lastApps = lastChallenge.getDailyChallenges().get(lastIndexOfDailyChallenge)
+                .getApps()
+                .stream()
+                .map(AppGoalTimeRequest::of)
+                .toList();
+
+        return lastApps;
     }
 
     public ChallengeResponse getChallenge(Long userId, String os) {
         val challenge = challengeRepository.findFirstByUserIdOrderByCreatedAtDesc(userId);
+
         return ChallengeResponse.of(challenge, os);
     }
 }
