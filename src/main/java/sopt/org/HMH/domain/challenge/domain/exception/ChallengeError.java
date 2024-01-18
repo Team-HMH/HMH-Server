bbff06b5974ba0f69a976be1bee12db7cc714b9c
임주민 <limj1213@gmail.com>
2024-01-18 17:45:18 +0900

    feat - #81 챌린지 관련 에러 메시지 추가

--- src/main/java/sopt/org/HMH/domain/challenge/domain/exception/ChallengeError.java
+++ src/main/java/sopt/org/HMH/domain/challenge/domain/exception/ChallengeError.java
@@ -8,6 +8,10 @@ import sopt.org.HMH.global.common.exception.base.ErrorBase;
 public enum ChallengeError implements ErrorBase {
 
     CHALLENGE_NOT_FOUND(HttpStatus.NOT_FOUND, "챌린지를 찾을 수 없습니다."),
+    INVALID_PERIOD_NULL(HttpStatus.BAD_REQUEST, "챌린지 기간은 null일 수 없습니다."),
+    INVALID_PERIOD_NUMERIC(HttpStatus.BAD_REQUEST, "유효한 숫자의 챌린지 기간을 입력해주세요."),
+    INVALID_GOAL_TIME_NULL(HttpStatus.BAD_REQUEST, "목표시간은 null일 수 없습니다."),
+    INVALID_GOAL_TIME_NUMERIC(HttpStatus.BAD_REQUEST, "유효한 숫자의 목표 시간을 입력해주세요."),
     ;
 
     private final HttpStatus status;
