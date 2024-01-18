ae28a3ca2f89212394eae14013210252ddee477a
transfer_kk <kseysh123@naver.com>
2024-01-19 04:13:06 +0900

    feat - #69 스웨거 설정 적용

--- src/main/java/sopt/org/HMH/domain/user/controller/UserApi.java
+++ src/main/java/sopt/org/HMH/domain/user/controller/UserApi.java
@@ -2,8 +2,6 @@ package sopt.org.HMH.domain.user.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -19,265 +17,41 @@ import sopt.org.HMH.global.common.response.BaseResponse;
 public interface UserApi {
 
     @Operation(
-            summary = "login API",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "요청이 성공했습니다."),
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "잘못된 요청입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "유효하지 않은 플랫폼 타입입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "애플 아이덴티티 토큰의 형식이 올바르지 않습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "애플 아이덴티티 토큰의 값이 올바르지 않습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "애플 로그인 중 퍼블릭 키 생성에 문제가 발생했습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "애플 로그인 중 아이덴티티 토큰의 유효 기간이 만료되었습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "애플 아이덴터티 토큰의 클레임 값이 올바르지 않습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "카카오 액세스 토큰의 정보를 조회하는 과정에서 오류가 발생하였습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "존재하지 않는 회원입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "405",
-                            description = "잘못된 HTTP method 요청입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "500",
-                            description = "서버 내부 오류입니다.",
-                            content = @Content)})
+            security = @SecurityRequirement(name = "Authorization"),
+            summary = "소셜 로그인")
     ResponseEntity<BaseResponse<?>> orderLogin(
-            @RequestHeader(JwtConstants.AUTHORIZATION) final String socialAccessToken,
+            @Parameter(hidden = true) @RequestHeader(JwtConstants.AUTHORIZATION) final String socialAccessToken,
             @RequestBody final SocialPlatformRequest request
     );
 
     @Operation(
-            summary = "signup API",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "201",
-                            description = "요청이 성공했습니다."),
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "잘못된 요청입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "유효하지 않은 플랫폼 타입입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "애플 아이덴티티 토큰의 형식이 올바르지 않습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "애플 아이덴티티 토큰의 값이 올바르지 않습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "애플 로그인 중 퍼블릭 키 생성에 문제가 발생했습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "애플 로그인 중 아이덴티티 토큰의 유효 기간이 만료되었습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "애플 아이덴터티 토큰의 클레임 값이 올바르지 않습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "카카오 액세스 토큰의 정보를 조회하는 과정에서 오류가 발생하였습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "405",
-                            description = "잘못된 HTTP method 요청입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "409",
-                            description = "이미 존재하는 회원입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "500",
-                            description = "서버 내부 오류입니다.",
-                            content = @Content)})
+            security = @SecurityRequirement(name = "Authorization"),
+            summary = "회원 가입")
     ResponseEntity<BaseResponse<?>> orderSignup(
-            @RequestHeader("Authorization") final String socialAccessToken,
+            @Parameter(hidden = true) @RequestHeader("Authorization") final String socialAccessToken,
             @RequestHeader("OS") final String os,
             @RequestBody final SocialSignUpRequest request
     );
 
     @Operation(
             security = @SecurityRequirement(name = "Authorization"),
-            summary = "token reissue API",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "요청이 성공했습니다."),
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "잘못된 요청입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "액세스 토큰의 형식이 올바르지 않습니다. Bearer 타입을 확인해 주세요.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "액세스 토큰의 값이 올바르지 않습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "액세스 토큰이 만료되었습니다. 재발급 받아주세요.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "존재하지 않는 회원입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "405",
-                            description = "잘못된 HTTP method 요청입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "500",
-                            description = "서버 내부 오류입니다.",
-                            content = @Content)})
+            summary = "토큰 재발급")
     ResponseEntity<BaseResponse<?>> orderReissue(
-            @RequestHeader("Authorization") final String refreshToken
+            @Parameter(hidden = true) @RequestHeader("Authorization") final String refreshToken
     );
 
     @Operation(
             security = @SecurityRequirement(name = "Authorization"),
-            summary = "logout API",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "요청이 성공했습니다."),
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "잘못된 요청입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "액세스 토큰의 형식이 올바르지 않습니다. Bearer 타입을 확인해 주세요.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "액세스 토큰의 값이 올바르지 않습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "액세스 토큰이 만료되었습니다. 재발급 받아주세요.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "존재하지 않는 회원입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "405",
-                            description = "잘못된 HTTP method 요청입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "500",
-                            description = "서버 내부 오류입니다.",
-                            content = @Content)})
-    ResponseEntity<BaseResponse<?>> orderLogout(@Parameter(hidden = true)
-                                            @UserId final Long userId);
+            summary = "로그아웃")
+    ResponseEntity<BaseResponse<?>> orderLogout(@UserId @Parameter(hidden = true) final Long userId);
 
     @Operation(
             security = @SecurityRequirement(name = "Authorization"),
-            summary = "회원 탈퇴 API",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "요청이 성공했습니다."),
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "잘못된 요청입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "액세스 토큰의 형식이 올바르지 않습니다. Bearer 타입을 확인해 주세요.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "액세스 토큰의 값이 올바르지 않습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "액세스 토큰이 만료되었습니다. 재발급 받아주세요.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "405",
-                            description = "잘못된 HTTP method 요청입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "500",
-                            description = "서버 내부 오류입니다.",
-                            content = @Content)})
-    ResponseEntity<BaseResponse<?>> orderGetUserInfo(@Parameter(hidden = true)
-                                             @UserId final Long userId);
+            summary = "유저 정보 불러오기")
+    ResponseEntity<BaseResponse<?>> orderGetUserInfo(@UserId @Parameter(hidden = true) final Long userId);
 
     @Operation(
-            summary = "JWT 재발급 API",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "요청이 성공했습니다."),
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "잘못된 요청입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "액세스 토큰의 형식이 올바르지 않습니다. Bearer 타입을 확인해 주세요.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "액세스 토큰의 값이 올바르지 않습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "액세스 토큰이 만료되었습니다. 재발급 받아주세요.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "존재하지 않는 회원입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "리프레쉬 토큰을 찾을 수 없습니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "405",
-                            description = "잘못된 HTTP method 요청입니다.",
-                            content = @Content),
-                    @ApiResponse(
-                            responseCode = "500",
-                            description = "서버 내부 오류입니다.",
-                            content = @Content)})
-    ResponseEntity<BaseResponse<?>> orderWithdraw(@Parameter(hidden = true)
-                                                  @UserId final Long userId);
+            security = @SecurityRequirement(name = "Authorization"),
+            summary = "회원 탈퇴")
+    ResponseEntity<BaseResponse<?>> orderWithdraw(@UserId @Parameter(hidden = true) final Long userId);
 }
