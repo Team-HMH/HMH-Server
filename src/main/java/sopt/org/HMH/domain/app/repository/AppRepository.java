5f31467703f86d2be13b9f6983870ecf17d276cf
임주민 <limj1213@gmail.com>
2024-01-19 22:04:43 +0900

    feat - #85 appRepository에서 앱 발견 못할 시 에러 처리

--- src/main/java/sopt/org/HMH/domain/app/repository/AppRepository.java
+++ src/main/java/sopt/org/HMH/domain/app/repository/AppRepository.java
@@ -2,8 +2,19 @@ package sopt.org.HMH.domain.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import sopt.org.HMH.domain.app.domain.App;
+import sopt.org.HMH.domain.app.domain.exception.AppError;
+import sopt.org.HMH.domain.app.domain.exception.AppException;
+
+import java.util.Optional;
 
 public interface AppRepository extends JpaRepository<App, Long> {
 
-    App findByDailyChallengeIdAndAppCodeAndOs(Long dailyChallengeId, String appCode, String os);
+    Optional<App> findFirstByDailyChallengeIdAndAppCodeAndOs(Long dailyChallengeId, String appCode, String os);
+
+    default App findFirstByDailyChallengeIdAndAppCodeAndOsOrElseThrow(Long dailyChallengeId, String appCode, String os) {
+        return findFirstByDailyChallengeIdAndAppCodeAndOs(dailyChallengeId, appCode, os).orElseThrow(() -> new AppException(
+                AppError.APP_NOT_FOUND));
+    }
+
+    boolean existsByDailyChallengeIdAndAppCodeAndOs(Long dailyChallengeId, String appCode, String os);
 }
