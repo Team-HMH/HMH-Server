601112bc1aa099a308625bfcc5bba010f7e7cca1
임주민 <limj1213@gmail.com>
2024-01-20 12:34:27 +0900

    refactor - #85 HttpStatus 수정

--- src/main/java/sopt/org/HMH/domain/app/domain/exception/AppError.java
+++ src/main/java/sopt/org/HMH/domain/app/domain/exception/AppError.java
@@ -8,8 +8,8 @@ import sopt.org.HMH.global.common.exception.base.ErrorBase;
 public enum AppError implements ErrorBase {
 
     APP_NOT_FOUND(HttpStatus.NOT_FOUND, "앱을 찾을 수 없습니다."),
-    APP_EXIST_ALREADY(HttpStatus.ALREADY_REPORTED, "이미 추가된 앱입니다."),
-    INVALID_APP_CODE_NULL(HttpStatus.BAD_REQUEST, "앱코드 값이 비어있습니다"),
+    APP_EXIST_ALREADY(HttpStatus.CONFLICT, "이미 추가된 앱입니다."),
+    INVALID_APP_CODE_NULL(HttpStatus.BAD_REQUEST, "앱 코드 값이 비어있습니다"),
     INVALID_TIME_RANGE(HttpStatus.BAD_REQUEST, "앱 시간의 범위가 유효한지 확인해주세요"),
     INVALID_TIME_NULL(HttpStatus.BAD_REQUEST, "앱 시간을 입력해주세요"),
     ;
