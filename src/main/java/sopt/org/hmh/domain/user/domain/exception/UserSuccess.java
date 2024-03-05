b33e7896521cf99622036a93edc84245a810dfde
kseysh <kseysh123@naver.com>
2024-03-05 18:55:01 +0900

    feat - #100 카카오 소셜 액세스 토큰 획득 controller 작성

--- src/main/java/sopt/org/hmh/domain/user/domain/exception/UserSuccess.java
+++ src/main/java/sopt/org/hmh/domain/user/domain/exception/UserSuccess.java
@@ -13,6 +13,7 @@ public enum UserSuccess implements SuccessBase {
     LOGOUT_SUCCESS(HttpStatus.OK, "로그아웃에 성공했습니다."),
     GET_USER_INFO_SUCCESS(HttpStatus.OK, "유저의 정보를 불러오는데에 성공했습니다."),
     WITHDRAW_SUCCESS(HttpStatus.OK, "회원 탈퇴를 성공하였습니다."),
+    GET_SOCIAL_ACCESS_TOKEN_SUCCESS(HttpStatus.OK, "소셜 액세스 토큰 발급을 성공했습니다."),
 
     // 201 CREATED
     SIGNUP_SUCCESS(HttpStatus.CREATED, "회원 가입에 성공했습니다."),
