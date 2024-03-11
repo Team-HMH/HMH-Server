ccec31b556d0e5ca5f67b714e81dc907e4c3b507
kseysh <kseysh123@naver.com>
2024-03-11 15:58:33 +0900

    refactor - #100 불필요한 공백 제거

--- src/main/java/sopt/org/hmh/global/auth/social/kakao/fegin/KakaoLoginService.java
+++ src/main/java/sopt/org/hmh/global/auth/social/kakao/fegin/KakaoLoginService.java
@@ -70,6 +70,5 @@ public class KakaoLoginService {
         return SocialAccessTokenResponse.builder()
                 .socialAccessToken(kakaoTokenResponse.accessToken())
                 .build();
-
     }
 }
