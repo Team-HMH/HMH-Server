149006aeacbbdacc517c0f30513bd4e4cc42d709
kseysh <kseysh123@naver.com>
2024-03-16 13:37:18 +0900

    feat - #111 테스트 폴더 gitignore에서 제외

---
+++ src/test/java/sopt/org/hmh/HmhApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.org.hmh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
