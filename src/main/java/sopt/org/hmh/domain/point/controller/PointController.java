4efa059cf3958a86e17cfc106a14eaab88fe1d78
kseysh <kseysh123@naver.com>
2024-05-02 21:12:47 +0900

    feat - #110 PointSuccess 추가

--- src/main/java/sopt/org/hmh/domain/point/controller/PointController.java
+++ src/main/java/sopt/org/hmh/domain/point/controller/PointController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sopt.org.hmh.domain.dailychallenge.domain.exception.DailyChallengeSuccess;
+import sopt.org.hmh.domain.point.dto.response.PointUsageResponse;
+import sopt.org.hmh.domain.point.exception.PointSuccess;
 import sopt.org.hmh.domain.point.service.PointFacade;
 import sopt.org.hmh.global.auth.UserId;
 import sopt.org.hmh.global.common.response.BaseResponse;
@@ -15,17 +16,18 @@ import sopt.org.hmh.global.common.response.BaseResponse;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/point")
-public class PointController {
+public class PointController implements PointApi {
 
     private final PointFacade pointFacade;
 
     @PatchMapping("/use")
-    public ResponseEntity<BaseResponse<?>> orderChallengeFailureByUsagePoint(
+    public ResponseEntity<BaseResponse<PointUsageResponse>> orderChallengeFailureByUsagePoint(
             @UserId final Long userId,
             @RequestBody final LocalDate challengeDate
     ) {
         return ResponseEntity
-                .status(DailyChallengeSuccess.MODIFY_DAILY_CHALLENGE_STATUS_FAILURE_SUCCESS.getHttpStatus())
-                .body(BaseResponse.success(DailyChallengeSuccess.MODIFY_DAILY_CHALLENGE_STATUS_FAILURE_SUCCESS, pointFacade.failChallengeByUsagePoint(userId, challengeDate)));
+                .status(PointSuccess.POINT_USAGE_SUCCESS.getHttpStatus())
+                .body(BaseResponse.success(PointSuccess.POINT_USAGE_SUCCESS,
+                        pointFacade.failChallengeByUsagePoint(userId, challengeDate)));
     }
 }
