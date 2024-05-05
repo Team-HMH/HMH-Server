8c21089ee02e5928dbcf91a78249e17a31d21042
kseysh <kseysh123@naver.com>
2024-05-05 18:13:38 +0900

    refactor - #120 app의 goalTime과 혼동을 우려하여 필드 네이밍 변경

---
+++ src/main/java/sopt/org/hmh/domain/dailychallenge/service/DailyChallengeFacade.java
@@ -0,0 +1,25 @@
+package sopt.org.hmh.domain.dailychallenge.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sopt.org.hmh.domain.app.service.AppService;
+import sopt.org.hmh.domain.dailychallenge.domain.DailyChallenge;
+import sopt.org.hmh.domain.dailychallenge.dto.request.FinishedDailyChallengeListRequest;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class DailyChallengeFacade {
+
+    private final DailyChallengeService dailyChallengeService;
+    private final AppService appService;
+
+    public void addFinishedDailyChallengeHistory(Long userId, FinishedDailyChallengeListRequest requests, String os) {
+        requests.finishedDailyChallenges().forEach(request -> {
+            DailyChallenge dailyChallenge = dailyChallengeService.findByChallengeDateAndUserIdOrThrowException(request.challengeDate(), userId);
+            dailyChallengeService.finishDailyChallengeByChangeStatus(dailyChallenge);
+            appService.addAppForHistory(request.apps(), dailyChallenge, os);
+        });
+    }
+}
