58af07fcba0303a809f3674f8da96c5acf3ed29c
kseysh <kseysh123@naver.com>
2024-05-05 17:18:24 +0900

    feat - #110 DailyChallenge Status별 Status 변환 로직 추가

--- src/main/java/sopt/org/hmh/domain/dailychallenge/domain/exception/DailyChallengeError.java
+++ src/main/java/sopt/org/hmh/domain/dailychallenge/domain/exception/DailyChallengeError.java
@@ -9,7 +9,7 @@ public enum DailyChallengeError implements ErrorBase {
 
     DAILY_CHALLENGE_NOT_FOUND(HttpStatus.NOT_FOUND, "일별 챌린지를 찾을 수 없습니다."),
     DAILY_CHALLENGE_YESTERDAY_NOT_FOUND(HttpStatus.NOT_FOUND, "어제의 일별 챌린지를 찾을 수 없습니다."),
-    DAILY_CHALLENGE_ALREADY_HANDLED(HttpStatus.BAD_REQUEST, "이미 처리된 일별 챌린지입니다.")
+    DAILY_CHALLENGE_ALREADY_PROCESSED(HttpStatus.BAD_REQUEST, "이미 처리된 일별 챌린지입니다.")
     ;
 
     private final HttpStatus status;
