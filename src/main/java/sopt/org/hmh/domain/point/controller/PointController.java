d1fc1db016a94893e410b57b4401663ea995cbb8
kseysh <kseysh123@naver.com>
2024-05-31 22:50:55 +0900

    fix - #151 LocalDate를 DTO에서 받도록 변경

--- src/main/java/sopt/org/hmh/domain/point/controller/PointController.java
+++ src/main/java/sopt/org/hmh/domain/point/controller/PointController.java
@@ -1,6 +1,5 @@
 package sopt.org.hmh.domain.point.controller;
 
-import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +7,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sopt.org.hmh.domain.point.dto.request.ChallengeDateRequest;
 import sopt.org.hmh.domain.point.dto.response.*;
 import sopt.org.hmh.domain.point.exception.PointSuccess;
 import sopt.org.hmh.domain.point.service.PointFacade;
@@ -37,24 +37,24 @@ public class PointController implements PointApi {
     @PatchMapping("/use")
     public ResponseEntity<BaseResponse<UsePointResponse>> orderUsagePointAndChallengeFailed(
             @UserId final Long userId,
-            @RequestBody final LocalDate challengeDate
+            @RequestBody final ChallengeDateRequest challengeDateRequest
     ) {
         return ResponseEntity
                 .status(PointSuccess.POINT_USAGE_SUCCESS.getHttpStatus())
                 .body(BaseResponse.success(PointSuccess.POINT_USAGE_SUCCESS,
-                        pointFacade.usePointAndChallengeFailed(userId, challengeDate)));
+                        pointFacade.usePointAndChallengeFailed(userId, challengeDateRequest.challengeDate())));
     }
 
     @Override
     @PatchMapping("/earn")
     public ResponseEntity<BaseResponse<EarnPointResponse>> orderEarnPointAndChallengeEarned(
             @UserId final Long userId,
-            @RequestBody final LocalDate challengeDate
+            @RequestBody final ChallengeDateRequest challengeDateRequest
     ) {
         return ResponseEntity
                 .status(PointSuccess.POINT_EARN_SUCCESS.getHttpStatus())
                 .body(BaseResponse.success(PointSuccess.POINT_EARN_SUCCESS,
-                        pointFacade.earnPointAndChallengeEarned(userId, challengeDate)));
+                        pointFacade.earnPointAndChallengeEarned(userId, challengeDateRequest.challengeDate())));
     }
 
     @Override
