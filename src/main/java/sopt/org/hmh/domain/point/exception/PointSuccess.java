23478a2f9035e1dd218124b91104486b30a06e18
kseysh <kseysh123@naver.com>
2024-06-09 20:25:00 +0900

    feat - #149 받을 포인트 반환 api 추가

--- src/main/java/sopt/org/hmh/domain/point/exception/PointSuccess.java
+++ src/main/java/sopt/org/hmh/domain/point/exception/PointSuccess.java
@@ -10,6 +10,7 @@ public enum PointSuccess implements SuccessBase {
     POINT_USAGE_SUCCESS(HttpStatus.OK, "포인트 사용에 성공하였습니다"),
     POINT_EARN_SUCCESS(HttpStatus.OK, "포인트 받기에 성공하였습니다"),
     GET_USAGE_POINT_SUCCESS(HttpStatus.OK, "사용할 포인트 반환에 성공하였습니다"),
+    GET_EARNED_POINT_SUCCESS(HttpStatus.OK, "받을 포인트 반환에 성공하였습니다"),
     GET_CHALLENGE_POINT_STATUS_LIST_SUCCESS(HttpStatus.OK, "챌린지 포인트 수령 여부 리스트 조회에 성공하였습니다."),
     ;
 
