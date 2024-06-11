6fcdd2152ee93eee14def2603308680fc74c4de5
kseysh <kseysh123@naver.com>
2024-06-11 23:47:36 +0900

    fix - #155 챌린지 최소 시간 수정

--- src/main/java/sopt/org/hmh/domain/challenge/domain/ChallengeConstants.java
+++ src/main/java/sopt/org/hmh/domain/challenge/domain/ChallengeConstants.java
@@ -5,7 +5,7 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public abstract class ChallengeConstants {
-    public static final Long MINIMUM_GOAL_TIME = 0L;
+    public static final Long MINIMUM_GOAL_TIME = 3_600_000L;
     public static final Long MAXIMUM_GOAL_TIME = 21_600_000L;
     public static final Integer USAGE_POINT = 100;
     public static final Integer EARNED_POINT = 20;
