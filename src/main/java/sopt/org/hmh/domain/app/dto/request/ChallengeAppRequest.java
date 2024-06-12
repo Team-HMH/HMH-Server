3f6659671e65d21a13c4bc911f809d2aaa80be6f
kseysh <kseysh123@naver.com>
2024-06-12 17:33:51 +0900

    refactor - #164 app time validation에서 어노테이션에서 상수만을 사용해야하여 생성자에서 validation을 진행하도록 수정

--- src/main/java/sopt/org/hmh/domain/app/dto/request/ChallengeAppRequest.java
+++ src/main/java/sopt/org/hmh/domain/app/dto/request/ChallengeAppRequest.java
@@ -1,17 +1,19 @@
 package sopt.org.hmh.domain.app.dto.request;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import sopt.org.hmh.domain.app.domain.AppConstants;
+import sopt.org.hmh.domain.app.domain.exception.AppError;
+import sopt.org.hmh.domain.app.domain.exception.AppException;
 
 public record ChallengeAppRequest(
         @NotNull(message = "앱 코드는 null일 수 없습니다.")
         String appCode,
         @NotNull(message = "앱 시간은 null일 수 없습니다.")
-        @Max(value = AppConstants.MAXIMUM_APP_TIME, message = "앱 시간은 최대 앱 시간 이상으로 설정할 수 없습니다.")
-        @Min(value = AppConstants.MINIMUM_APP_TIME, message = "앱 시간은 최소 앱 시간 이하로 설정할 수 없습니다.")
         Long goalTime
 ) {
-
+        public ChallengeAppRequest {
+                if (goalTime > AppConstants.MAXIMUM_APP_TIME || goalTime < AppConstants.MINIMUM_APP_TIME) {
+                        throw new AppException(AppError.INVALID_TIME_RANGE);
+                }
+        }
 }
--- src/main/java/sopt/org/hmh/domain/challenge/dto/request/ChallengeSignUpRequest.java
+++ src/main/java/sopt/org/hmh/domain/challenge/dto/request/ChallengeSignUpRequest.java
@@ -1,20 +1,23 @@
 package sopt.org.hmh.domain.challenge.dto.request;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import sopt.org.hmh.domain.app.domain.AppConstants;
+import sopt.org.hmh.domain.app.domain.exception.AppError;
+import sopt.org.hmh.domain.app.domain.exception.AppException;
 import sopt.org.hmh.domain.app.dto.request.ChallengeAppRequest;
 
 import java.util.List;
-import sopt.org.hmh.domain.challenge.domain.ChallengeConstants;
 
 public record ChallengeSignUpRequest(
         @NotNull(message = "챌린지 기간은 null일 수 없습니다.")
         Integer period,
-        @Max(value = ChallengeConstants.MAXIMUM_GOAL_TIME, message = "챌린지 목표 시간은 최대 목표 시간 이상으로 설정할 수 없습니다.")
-        @Min(value = ChallengeConstants.MINIMUM_GOAL_TIME, message = "챌린지 목표 시간은 최소 목표 시간 이하로 설정할 수 없습니다.")
         @NotNull(message = "챌린지 목표시간은 null일 수 없습니다.")
         Long goalTime,
         List<ChallengeAppRequest> apps
 ) {
+        public ChallengeSignUpRequest {
+                if (goalTime > AppConstants.MAXIMUM_APP_TIME || goalTime < AppConstants.MINIMUM_APP_TIME) {
+                        throw new AppException(AppError.INVALID_TIME_RANGE);
+                }
+        }
 }
