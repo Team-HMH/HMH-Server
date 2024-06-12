4110639de22b67185d62b91e74d52a5b4878b135
kseysh <kseysh123@naver.com>
2024-06-12 17:17:13 +0900

    refactor - #164 request dto 제약조건 추가

--- src/main/java/sopt/org/hmh/domain/challenge/dto/request/ChallengeSignUpRequest.java
+++ src/main/java/sopt/org/hmh/domain/challenge/dto/request/ChallengeSignUpRequest.java
@@ -1,11 +1,19 @@
 package sopt.org.hmh.domain.challenge.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import sopt.org.hmh.domain.app.dto.request.ChallengeAppRequest;
 
 import java.util.List;
+import sopt.org.hmh.domain.challenge.domain.ChallengeConstants;
 
 public record ChallengeSignUpRequest(
+        @NotNull(message = "챌린지 기간은 null일 수 없습니다.")
         Integer period,
+        @Max(value = ChallengeConstants.MAXIMUM_GOAL_TIME, message = "챌린지 목표 시간은 최대 목표 시간 이상으로 설정할 수 없습니다.")
+        @Min(value = ChallengeConstants.MINIMUM_GOAL_TIME, message = "챌린지 목표 시간은 최소 목표 시간 이하로 설정할 수 없습니다.")
+        @NotNull(message = "챌린지 목표시간은 null일 수 없습니다.")
         Long goalTime,
         List<ChallengeAppRequest> apps
 ) {
