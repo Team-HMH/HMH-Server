de1ba60e93ccbfd4d3e2e78360491ef24e15d477
kseysh <kseysh123@naver.com>
2024-06-13 16:17:05 +0900

    feat - #166 최근 lock 날짜 변경 controller 추가

--- src/main/java/sopt/org/hmh/domain/user/controller/UserApi.java
+++ src/main/java/sopt/org/hmh/domain/user/controller/UserApi.java
@@ -3,22 +3,29 @@ package sopt.org.hmh.domain.user.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
-import sopt.org.hmh.global.auth.UserId;
+import sopt.org.hmh.domain.user.dto.request.UserRequest.LockDateRequest;
+import sopt.org.hmh.domain.user.dto.response.UserInfoResponse;
 import sopt.org.hmh.global.common.response.BaseResponse;
+import sopt.org.hmh.global.common.response.EmptyJsonResponse;
 
 public interface UserApi {
 
     @Operation(summary = "로그아웃")
-    ResponseEntity<BaseResponse<?>> orderLogout();
+    ResponseEntity<BaseResponse<EmptyJsonResponse>> orderLogout();
 
     @Operation(summary = "유저 정보 불러오기")
-    ResponseEntity<BaseResponse<?>> orderGetUserInfo(@UserId @Parameter(hidden = true) final Long userId);
+    ResponseEntity<BaseResponse<UserInfoResponse>> orderGetUserInfo(@Parameter(hidden = true) final Long userId);
 
     @Operation(summary = "유저 포인트 정보 불러오기")
-    public ResponseEntity<BaseResponse<?>> orderGetUserPoint(@UserId final Long userId);
+    ResponseEntity<BaseResponse<Integer>> orderGetUserPoint(@Parameter(hidden = true) final Long userId);
 
     @Operation(
             summary = "회원 탈퇴")
-    ResponseEntity<BaseResponse<?>> orderWithdraw(@UserId @Parameter(hidden = true) final Long userId);
+    ResponseEntity<BaseResponse<EmptyJsonResponse>> orderWithdraw(@Parameter(hidden = true) final Long userId);
+
+    @Operation(
+            summary = "당일 잠금 여부 전송")
+    ResponseEntity<BaseResponse<EmptyJsonResponse>> orderChangeRecentLockDate(
+            @Parameter(hidden = true) final Long userId, final LockDateRequest request);
 
 }
--- src/main/java/sopt/org/hmh/domain/user/controller/UserController.java
+++ src/main/java/sopt/org/hmh/domain/user/controller/UserController.java
@@ -5,9 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sopt.org.hmh.domain.user.domain.exception.UserSuccess;
+import sopt.org.hmh.domain.user.dto.request.UserRequest.LockDateRequest;
+import sopt.org.hmh.domain.user.dto.response.UserInfoResponse;
 import sopt.org.hmh.domain.user.service.UserService;
 import sopt.org.hmh.global.auth.UserId;
 import sopt.org.hmh.global.common.response.BaseResponse;
@@ -22,7 +25,7 @@ public class UserController implements UserApi{
 
     @PostMapping("/logout")
     @Override
-    public ResponseEntity<BaseResponse<?>> orderLogout() {
+    public ResponseEntity<BaseResponse<EmptyJsonResponse>> orderLogout() {
         return ResponseEntity
                 .status(UserSuccess.LOGOUT_SUCCESS.getHttpStatus())
                 .body(BaseResponse.success(UserSuccess.LOGOUT_SUCCESS, new EmptyJsonResponse()));
@@ -30,7 +33,7 @@ public class UserController implements UserApi{
 
     @GetMapping
     @Override
-    public ResponseEntity<BaseResponse<?>> orderGetUserInfo(@UserId final Long userId) {
+    public ResponseEntity<BaseResponse<UserInfoResponse>> orderGetUserInfo(@UserId final Long userId) {
         return ResponseEntity
                 .status(UserSuccess.GET_USER_INFO_SUCCESS.getHttpStatus())
                 .body(BaseResponse.success(UserSuccess.GET_USER_INFO_SUCCESS, userService.getUserInfo(userId)));
@@ -38,18 +41,28 @@ public class UserController implements UserApi{
 
     @GetMapping("/point")
     @Override
-    public ResponseEntity<BaseResponse<?>> orderGetUserPoint(@UserId final Long userId) {
+    public ResponseEntity<BaseResponse<Integer>> orderGetUserPoint(@UserId final Long userId) {
         return ResponseEntity
                 .status(UserSuccess.GET_USER_POINT_SUCCESS.getHttpStatus())
                 .body(BaseResponse.success(UserSuccess.GET_USER_POINT_SUCCESS, userService.getUserInfo(userId).point()));
     }
 
     @DeleteMapping
-    public ResponseEntity<BaseResponse<?>> orderWithdraw(@UserId final Long userId) {
+    public ResponseEntity<BaseResponse<EmptyJsonResponse>> orderWithdraw(@UserId final Long userId) {
         userService.withdraw(userId);
         return ResponseEntity
                 .status(UserSuccess.WITHDRAW_SUCCESS.getHttpStatus())
                 .body(BaseResponse.success(UserSuccess.WITHDRAW_SUCCESS, new EmptyJsonResponse()));
+    }
+
+    @PostMapping("/daily/lock")
+    @Override
+    public ResponseEntity<BaseResponse<EmptyJsonResponse>> orderChangeRecentLockDate(
+            @UserId final Long userId, @RequestBody final LockDateRequest request) {
+        userService.changeRecentLockDate(userId, request.lockDate());
+        return ResponseEntity
+                .status(UserSuccess.GET_USER_POINT_SUCCESS.getHttpStatus())
+                .body(BaseResponse.success(UserSuccess.GET_USER_POINT_SUCCESS,new EmptyJsonResponse()));
     }
 
 }
