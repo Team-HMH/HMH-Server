6df6ab697c6bce53fe91781fbafccf385a60d940
kseysh <kseysh123@naver.com>
2024-06-13 16:17:49 +0900

    feat - #166 lockDateRequest 추가

---
+++ src/main/java/sopt/org/hmh/domain/user/dto/request/UserRequest.java
@@ -0,0 +1,16 @@
+package sopt.org.hmh.domain.user.dto.request;
+
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserRequest {
+
+    public record LockDateRequest(
+            @DateTimeFormat(pattern = "yyyy-MM-dd")
+            LocalDate lockDate
+    ) {
+    }
+}
