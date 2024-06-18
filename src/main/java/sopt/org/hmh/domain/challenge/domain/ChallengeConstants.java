bfc6325fea34a6ee4a5692014200f536b090879d
kseysh <kseysh123@naver.com>
2024-06-18 15:41:39 +0900

    refactor - #166 사용되지 않는 상수 값 삭제

--- src/main/java/sopt/org/hmh/domain/challenge/domain/ChallengeConstants.java
+++ src/main/java/sopt/org/hmh/domain/challenge/domain/ChallengeConstants.java
@@ -6,8 +6,6 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public abstract class ChallengeConstants {
-    public static final Long MINIMUM_GOAL_TIME = 3_600_000L;
-    public static final Long MAXIMUM_GOAL_TIME = 21_600_000L;
     public static final Integer USAGE_POINT = 100;
     public static final Integer EARNED_POINT = 20;
     public static final List<Integer> AVAILABLE_CHALLENGE_PERIODS = List.of(7, 14, 20, 30);
