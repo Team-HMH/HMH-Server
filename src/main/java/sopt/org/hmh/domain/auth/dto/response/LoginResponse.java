78a12bfa649eced625d54d5c53ac7fd266795456
kseysh <kseysh123@naver.com>
2024-06-19 03:20:20 +0900

    refactor - #122 불필요한 of 함수 제거

--- src/main/java/sopt/org/hmh/domain/auth/dto/response/LoginResponse.java
+++ src/main/java/sopt/org/hmh/domain/auth/dto/response/LoginResponse.java
@@ -1,7 +1,6 @@
 package sopt.org.hmh.domain.auth.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import sopt.org.hmh.domain.user.domain.User;
 import sopt.org.hmh.global.auth.jwt.TokenResponse;
 
 public record LoginResponse(
@@ -9,12 +8,4 @@ public record LoginResponse(
         @JsonProperty(value = "token")
         TokenResponse tokenResponse
 ){
-
-    public static LoginResponse of(User loginUser, TokenResponse tokenResponse) {
-        return new LoginResponse(
-                loginUser.getId(),
-                tokenResponse
-        );
-    }
-
 }
