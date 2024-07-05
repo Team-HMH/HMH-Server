a98eb47f8535e077cd37be35ab4cf7452fedaf2b
kseysh <kseysh123@naver.com>
2024-07-06 05:27:20 +0900

    feat - #171 admin 토큰인지 확인하는 interceptor 추가

--- src/main/java/sopt/org/hmh/global/config/WebConfig.java
+++ src/main/java/sopt/org/hmh/global/config/WebConfig.java
@@ -1,14 +1,22 @@
 package sopt.org.hmh.global.config;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import sopt.org.hmh.global.auth.UserIdArgumentResolver;
+import sopt.org.hmh.global.auth.security.ValidateAdminInterceptor;
 
 @Configuration
+@EnableWebMvc
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final ValidateAdminInterceptor validateAdminInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -22,5 +30,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new UserIdArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(validateAdminInterceptor)
+                .addPathPatterns("/api/v1/admin/**")
+                .excludePathPatterns("/api/v1/admin/login");
     }
 }
