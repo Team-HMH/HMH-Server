8bb97903910d824fd47a2253effbe6dfd7d570dd
kseysh <kseysh123@naver.com>
2024-07-07 18:09:33 +0900

    feat - #171 challenge id로 dailyChallenge를 찾는 로직 추가

--- src/main/java/sopt/org/hmh/domain/dailychallenge/repository/DailyChallengeRepository.java
+++ src/main/java/sopt/org/hmh/domain/dailychallenge/repository/DailyChallengeRepository.java
@@ -1,10 +1,14 @@
 package sopt.org.hmh.domain.dailychallenge.repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sopt.org.hmh.domain.dailychallenge.domain.DailyChallenge;
 
 public interface DailyChallengeRepository extends JpaRepository<DailyChallenge, Long> {
+
     Optional<DailyChallenge> findByChallengeDateAndUserId(LocalDate challengeDate, Long userId);
+
+    List<DailyChallenge> findAllByChallengeId(Long challengeId);
 }
