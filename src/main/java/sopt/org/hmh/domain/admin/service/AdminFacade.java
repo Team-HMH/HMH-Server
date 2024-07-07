a1b41fbfe0be18ec363352c87ca9e6c565a137df
kseysh <kseysh123@naver.com>
2024-07-07 18:10:48 +0900

    feat - #171 dailyChallenge 정보를 변경하는 facade 로직 추가

--- src/main/java/sopt/org/hmh/domain/admin/service/AdminFacade.java
+++ src/main/java/sopt/org/hmh/domain/admin/service/AdminFacade.java
@@ -1,14 +1,22 @@
 package sopt.org.hmh.domain.admin.service;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sopt.org.hmh.domain.admin.dto.request.AdminDailyChallengeRequest;
 import sopt.org.hmh.domain.admin.dto.request.AdminUserInfoRequest;
 import sopt.org.hmh.domain.admin.dto.response.AdminTokenResponse;
 import sopt.org.hmh.domain.admin.exception.AdminError;
 import sopt.org.hmh.domain.admin.exception.AdminException;
+import sopt.org.hmh.domain.challenge.domain.exception.ChallengeError;
+import sopt.org.hmh.domain.challenge.domain.exception.ChallengeException;
+import sopt.org.hmh.domain.challenge.service.ChallengeService;
+import sopt.org.hmh.domain.dailychallenge.domain.Status;
+import sopt.org.hmh.domain.dailychallenge.service.DailyChallengeService;
 import sopt.org.hmh.domain.user.domain.User;
 import sopt.org.hmh.domain.user.service.UserService;
 import sopt.org.hmh.global.auth.jwt.TokenService;
@@ -22,6 +30,8 @@ public class AdminFacade {
 
     private final UserService userService;
     private final TokenService tokenService;
+    private final ChallengeService challengeService;
+    private final DailyChallengeService dailyChallengeService;
 
     public AdminTokenResponse adminLogin(String authCode) {
         validateAdminAuthCode(authCode);
@@ -48,6 +58,23 @@ public class AdminFacade {
         }
         if (Objects.nonNull(request.name())) {
             user.changeName(request.name());
+        }
+    }
+
+    @Transactional
+    public void changeDailyChallengeInfo(AdminDailyChallengeRequest request) {
+        Long currentChallengeId = userService.getCurrentChallengeIdByUserId(request.userId());
+        List<Status> statuses = request.statuses();
+        LocalDate challengeDate = request.startDate();
+
+        validateStatusesPeriod(currentChallengeId, statuses);
+        dailyChallengeService.changeInfoOfDailyChallenges(currentChallengeId, statuses, challengeDate);
+    }
+
+    private void validateStatusesPeriod(Long challengeId, List<Status> statuses) {
+        Integer challengePeriod = challengeService.getChallengePeriod(challengeId);
+        if (challengePeriod != statuses.size()) {
+            throw new ChallengeException(ChallengeError.INVALID_PERIOD_NUMERIC);
         }
     }
 }
