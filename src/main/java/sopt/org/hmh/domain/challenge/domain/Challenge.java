63e78bbf5268c39f24c0a5caf851e75a97a56917
kseysh <kseysh123@naver.com>
2024-07-17 21:03:38 +0900

    feat - #175 Challenge 도메인에 startDate 추가

--- src/main/java/sopt/org/hmh/domain/challenge/domain/Challenge.java
+++ src/main/java/sopt/org/hmh/domain/challenge/domain/Challenge.java
@@ -4,6 +4,7 @@ import static jakarta.persistence.GenerationType.*;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,7 +12,6 @@ import lombok.NoArgsConstructor;
 import sopt.org.hmh.domain.app.domain.ChallengeApp;
 import sopt.org.hmh.global.common.domain.BaseTimeEntity;
 import sopt.org.hmh.domain.dailychallenge.domain.DailyChallenge;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -32,6 +32,9 @@ public class Challenge extends BaseTimeEntity {
     @NotNull(message = "목표 시간은 null일 수 없습니다.")
     private Long goalTime;
 
+    @NotNull(message = "시작 날짜는 null일 수 없습니다.")
+    private LocalDate startDate;
+
     @OneToMany(mappedBy = "challenge", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<ChallengeApp> apps;
 
@@ -39,10 +42,11 @@ public class Challenge extends BaseTimeEntity {
     private List<DailyChallenge> historyDailyChallenges;
 
     @Builder
-    private Challenge(Integer period, Long userId, Long goalTime, List<ChallengeApp> apps) {
+    private Challenge(Integer period, Long userId, Long goalTime, List<ChallengeApp> apps, LocalDate startDate) {
         this.period = period;
         this.userId = userId;
         this.goalTime = goalTime;
         this.apps = apps;
+        this.startDate = startDate;
     }
 }
