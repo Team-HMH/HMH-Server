76b5b4ee04113a39a34ba2c6166630399ae14f0b
kseysh <kseysh123@naver.com>
2024-07-18 15:48:27 +0900

    refactor - #175 challengeRepository 추상화

--- src/main/java/sopt/org/hmh/domain/challenge/repository/ChallengeJpaRepository.java
+++ src/main/java/sopt/org/hmh/domain/challenge/repository/ChallengeJpaRepository.java
@@ -2,9 +2,11 @@ package sopt.org.hmh.domain.challenge.repository;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import sopt.org.hmh.domain.challenge.domain.Challenge;
 
-public interface ChallengeRepository {
+public interface ChallengeJpaRepository extends JpaRepository<Challenge, Long> {
+
     Optional<Challenge> findById(Long id);
 
     void deleteByUserIdIn(List<Long> userId);
---
+++ src/main/java/sopt/org/hmh/domain/challenge/repository/ChallengeRepositoryImpl.java
@@ -0,0 +1,29 @@
+package sopt.org.hmh.domain.challenge.repository;
+
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import sopt.org.hmh.domain.challenge.domain.Challenge;
+
+@Repository
+@RequiredArgsConstructor
+public class ChallengeRepositoryImpl implements ChallengeRepository{
+
+    private final ChallengeJpaRepository challengeJpaRepository;
+
+    @Override
+    public Optional<Challenge> findById(Long id) {
+        return challengeJpaRepository.findById(id);
+    }
+
+    @Override
+    public void deleteByUserIdIn(List<Long> userId) {
+        challengeJpaRepository.deleteByUserIdIn(userId);
+    }
+
+    @Override
+    public void deleteByUserId(Long userId) {
+        challengeJpaRepository.deleteByUserId(userId);
+    }
+}
