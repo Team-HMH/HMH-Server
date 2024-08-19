eb1532fe7c142f3d4f5fc224d78854c44c295611
임주민 <limj1213@gmail.com>
2024-08-19 10:31:14 +0900

    feat - #188 Error 메시지 추가

--- src/main/java/sopt/org/hmh/domain/dailychallenge/domain/exception/DailyChallengeError.java
+++ src/main/java/sopt/org/hmh/domain/dailychallenge/domain/exception/DailyChallengeError.java
@@ -10,7 +10,8 @@ public enum DailyChallengeError implements ErrorBase {
     DAILY_CHALLENGE_NOT_FOUND(HttpStatus.NOT_FOUND, "일별 챌린지를 찾을 수 없습니다."),
     DAILY_CHALLENGE_PERIOD_INDEX_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 인덱스의 일별 챌린지를 찾을 수 없습니다."),
     DAILY_CHALLENGE_ALREADY_PROCESSED(HttpStatus.BAD_REQUEST, "이미 처리된 일별 챌린지입니다."),
-    DAILY_CHALLENGE_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 존재하는 일별 챌린지입니다.");
+    DAILY_CHALLENGE_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 존재하는 일별 챌린지입니다."),
+    PERIOD_INDEX_NOT_VALID(HttpStatus.BAD_REQUEST, "지난 날의 정보만 전송할 수 있습니다.");
 
     private final HttpStatus status;
     private final String errorMessage;
