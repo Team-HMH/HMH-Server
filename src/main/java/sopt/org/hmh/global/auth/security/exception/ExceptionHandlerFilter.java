2aa57a116a9883dddb8561444ec11f4a75235629
임주민 <limj1213@gmail.com>
2024-08-19 18:31:49 +0900

    feat - #160 서버 500에러 발생 시 슬랙 알림 전송

--- src/main/java/sopt/org/hmh/global/auth/security/exception/ExceptionHandlerFilter.java
+++ src/main/java/sopt/org/hmh/global/auth/security/exception/ExceptionHandlerFilter.java
@@ -4,13 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import sopt.org.hmh.domain.slack.SlackSender;
 import sopt.org.hmh.global.auth.jwt.JwtConstants;
 import sopt.org.hmh.global.auth.jwt.exception.JwtError;
 import sopt.org.hmh.global.auth.jwt.exception.JwtException;
@@ -18,8 +22,11 @@ import sopt.org.hmh.global.common.exception.base.ErrorBase;
 import sopt.org.hmh.global.common.response.BaseResponse;
 
 @Slf4j
+@RequiredArgsConstructor
+@Component
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final SlackSender slackSender;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException {
@@ -28,7 +35,7 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
         } catch (JwtException error) {
             handleUnauthorizedException(response, error.getError());
         } catch (Exception error) {
-            handleException(response, error);
+            handleException(response, request, error);
         }
     }
 
@@ -38,9 +45,10 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
         setResponse(response, httpStatus, jwtError);
     }
 
-    private void handleException(HttpServletResponse response, Exception e) throws IOException {
+    private void handleException(HttpServletResponse response, HttpServletRequest request, Exception e) throws IOException {
         log.error(">>> Exception Handler Filter : ", e);
         setResponse(response, HttpStatus.INTERNAL_SERVER_ERROR, JwtError.INTERNAL_SERVER_ERROR);
+        slackSender.sendNotificationServerInternalError(e, request);
     }
 
     private void setResponse(HttpServletResponse response, HttpStatus httpStatus, ErrorBase errorMessage) throws IOException {
