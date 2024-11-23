3213ca6ec1693ce423fed03ddacecb876d4a29bc
임주민 <limj1213@gmail.com>
2024-11-23 12:27:11 +0900

    feat - #199 Banner Response에 backgroundColors 추가

--- src/main/java/sopt/org/hmh/domain/banner/BannerResponse.java
+++ src/main/java/sopt/org/hmh/domain/banner/BannerResponse.java
@@ -2,12 +2,17 @@ package sopt.org.hmh.domain.banner;
 
 import lombok.Builder;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Builder
 public record BannerResponse(
         String title,
         String subTitle,
         String imageUrl,
-        String linkUrl
+        String linkUrl,
+        List<String> backgroundColors
+
 ) {
     public static BannerResponse of(Banner banner) {
         return BannerResponse.builder()
